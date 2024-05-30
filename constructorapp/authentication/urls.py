4a0dff9f5fafedd219aee5bdff19ebf6f7f69ca8
Juan <juan.m.garcia@correounivalle.edu.co>
2024-05-29 21:36:06 -0500

    creando la validacion del usuario

--- constructorapp/authentication/urls.py
+++ constructorapp/authentication/urls.py
@@ -1,7 +1,8 @@
-from .views  import RegistrationView
+from .views  import RegistrationView, UsernameValidationView
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt #se importa el decorador csrf_exempt que permite que la vista no requiera un token de seguridad
 
 urlpatterns = [
-  path('register', RegistrationView.as_view(), name="register")
-
+  path('register', RegistrationView.as_view(), name="register"),
+  path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name="validate-username")
 ]
--- constructorapp/authentication/views.py
+++ constructorapp/authentication/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.views import View
-
+import json
+from django.http import JsonResponse
 # Create your views here.
 
+#la siguiente clase se encarga de validar el username, en caso de que no sea alfanumerico se retorna un mensaje de error.
+class UsernameValidationView(View):
+  def post(self, request):#se recibe la peticion
+    data=json.loads(request.body) #se convierte el json a un diccionario
+    username=data['username']#se obtiene el username
+
+    if not str(username).isalnum():#se verifica si el username es alfanumerico
+      return JsonResponse({'username_error': 'El nombre de usuario solo debe tener caracteres alfanuemericos'}, status=400)
+    
+    if not str(username).isalnum():#se verifica si el username es alfanumerico
+      return JsonResponse({'username_error': 'El nombre de usuario solo debe tener caracteres alfanuemericos'}, status=400)
+    
+    
+    
+    
+    
+    
+    return JsonResponse({'username_valid': True})
+  
+  #objetivo es la comuncacuoin entre el front y el back por medio de ajax para saber si se puede o no crear un usuario
+  
 class RegistrationView(View):
   """
   A view for handling user registration.
