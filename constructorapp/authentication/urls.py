cbe11aa7d0203485ccc375f95f2139061426d784
Juan <juan.m.garcia@correounivalle.edu.co>
2024-06-04 12:32:14 -0500

    validacion email

--- constructorapp/authentication/urls.py
+++ constructorapp/authentication/urls.py
@@ -1,8 +1,11 @@
-from .views  import RegistrationView, UsernameValidationView
+from .views  import RegistrationView, UsernameValidationView, EmailValidationView
 from django.urls import path
-from django.views.decorators.csrf import csrf_exempt #se importa el decorador csrf_exempt que permite que la vista no requiera un token de seguridad
+from django.views.decorators.csrf import csrf_exempt#se importa el decorador csrf_exempt que permite que la vista no requiera un token de seguridad
 
 urlpatterns = [
   path('register', RegistrationView.as_view(), name="register"),
-  path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name="validate-username")
+  path('validate-username', csrf_exempt(UsernameValidationView.as_view()), #csrf_exempt se encarga de deshabilitar la proteccion csrf, lo cual permite que la vista pueda ser accedida sin un token de seguridad
+      name="validate-username"),
+  path('validate-email', csrf_exempt(EmailValidationView.as_view()), 
+      name="validate-email")
 ]
--- constructorapp/authentication/views.py
+++ constructorapp/authentication/views.py
@@ -4,22 +4,37 @@ import json #se importa la libreria json que permite trabajar con archivos json
 from django.http import JsonResponse #se importa la clase JsonResponse que permite retornar una respuesta en formato json
 from django.contrib.auth.models import User #se importa el modelo User de la aplicacion auth de django. Permite interactuar con la tabla de usuarios de la base de datos
 # Create your views here.
+from validate_email import validate_email #se importa la funcion validate_email que permite validar si un email es valido
 
-#la siguiente clase se encarga de validar el username, en caso de que no sea alfanumerico se retorna un mensaje de error.
-class UsernameValidationView(View):
+#la siguiente clase se encarga de validar el email. Si el email no es valido, se retorna un mensaje de error. Si el email ya esta en uso, se retorna un mensaje de error
+class EmailValidationView(View):
+  #objetivo es comunicar el front y el back por medio de ajax para saber si se puede o no registrar un email
   def post(self, request):#se recibe la peticion
     data=json.loads(request.body) #se convierte el json a un diccionario
-    username=data['username']#se obtiene el username
+    email=data['email']#se obtiene el email
+
+    if not validate_email(email):#se verifica si el email es valido
+      return JsonResponse({'email_error': 'Email invalido'}, status=400)
+    
+    if User.objects.filter(email=email).exists(): #se verifica si el email ya existe en la base de datos
+      return JsonResponse({'email_error': 'El email ya esta en uso, escoja otro'}, status=409)
+    
+    return JsonResponse({'username_valid': True})
+  
+class UsernameValidationView(View):
+  #objetivo es comunicar el front y el back por medio de ajax para saber si se puede o no crear un usuario
+  def post(self, request):#se recibe la peticion
+    data=json.loads(request.body) #se convierte el json a un diccionario
+    username=data['username']#se obtiene el email
 
     if not str(username).isalnum():#se verifica si el username es alfanumerico
-      return JsonResponse({'username_error': 'El nombre de usuario solo debe tener caracteres alfanuemericos'}, status=400)
+      return JsonResponse({'username_error': 'El nombre de usuario solo debe tener caracteres alfanumericos'}, status=400)
     
     if User.objects.filter(username=username).exists(): #se verifica si el username ya existe en la base de datos
       return JsonResponse({'username_error': 'El nombre de usuario ya existe, escoja otro'}, status=409)
     
     return JsonResponse({'username_valid': True})
   
-  #objetivo es comunicar el front y el back por medio de ajax para saber si se puede o no crear un usuario
   
 class RegistrationView(View):
   """
